c3a420bc2f565493df8e86c4abb81efdf8ead969
kimia <kim_hszd@hotmail.com>
2011-12-14 12:58:37 -0700

    title method added to helper

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'f410ab418d5f4684b9ddcb79fff23758cf01aad45f828a38a25f3f1327beede61836e97a94d7f6628c25fd610fefe635fe4c437e35d0cc15644fa1d1cebebe84'
